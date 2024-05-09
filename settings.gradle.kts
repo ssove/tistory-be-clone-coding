ac2860ca3110d278987808dd7379959a2d0f45fa
Serendipity <serendipity@Serendipityui-MacBookAir.local>
2024-05-10 00:51:28 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "tistory-backend-clone-coding"
---
+++ src/main/kotlin/com/example/tistorybackendclonecoding/TistoryBackendCloneCodingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tistorybackendclonecoding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TistoryBackendCloneCodingApplication
+
+fun main(args: Array<String>) {
+	runApplication<TistoryBackendCloneCodingApplication>(*args)
+}
