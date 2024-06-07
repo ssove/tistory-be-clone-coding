bf399e86643326d495a3d40f11604dcf1fe6fad9
Minseok Lee <korpenta@gmail.com>
2024-06-07 23:50:42 +0900

    Set env

---
+++ src/test/kotlin/com/example/tistorybackendclonecoding/TistoryBackendCloneCodingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tistorybackendclonecoding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TistoryBackendCloneCodingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
